8ad4168c61f55baeca49c8aa63a457fe21870c71
sivaldo vieira <sivaldovieiradealmeida@gmail.com>
2025-03-18 11:15:46 -0300

    modulo de novos operadores

---
+++ encadeOp.js
@@ -0,0 +1,12 @@
+const person = {
+  name: "Daniel",
+  surnaname: "Silva",
+  age: "30",
+  postalColde: "12345-230",
+  mother: {
+    name: "Maria",
+    surnaname: "silva",
+  },
+};
+
+console.log(person?.mother?.name);
