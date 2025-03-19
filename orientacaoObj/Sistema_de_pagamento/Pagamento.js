1c21d5f72cecd91788b15732a75f00f9f07ab016
sivaldo vieira <sivaldovieiradealmeida@gmail.com>
2025-03-19 18:19:45 -0300

    Polimorfismo - parte 2

---
+++ orientacaoObj/Sistema_de_pagamento/Pagamento.js
@@ -0,0 +1,11 @@
+export class Pagamento {
+  constructor(valor) {
+    this.valor = valor;
+  }
+
+  realizarPagamento() {
+    console.log("Processando Pagamento...");
+  }
+}
+
+//primeiro - classe mae vai ser herdada
---
+++ orientacaoObj/Sistema_de_pagamento/main.js
@@ -0,0 +1,17 @@
+import { CartaoDeCredito } from "./CartaoDeCredito.js";
+import { Pix } from "./Pix.js";
+import { Boleto } from "./BoletoBancario.js";
+
+const cartaoDecredito = new CartaoDeCredito(
+  150,
+  "1234 5678 9012",
+  "João Silva"
+);
+const pix = new Pix(200, "123.456.789-00");
+const boletoBancario = new Boleto(100, "12345678 890123 234567 0 24456");
+
+cartaoDecredito.realizarPagamento();
+pix.realizarPagamento();
+boletoBancario.realizarPagamento();
+
+//quinto - principal cria as instancias das outras classes
